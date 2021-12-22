cea8a6d63555a772c87c1bdab484040164450392
Jackie Jones <jonesjackie1994@gmail.com>
2021-12-21 19:19:54 -0800

    Components Part 1

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
+//import './App.css';
+import React, { Component } from 'react';
+import { Navbar, NavbarBrand } from 'reactstrap';
 import './App.css';
 
-function App() {
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+                <Navbar dark color="primary">
+                <div className="container">
+                    <NavbarBrand href="/">NuCamp</NavbarBrand>
+                </div>
+                </Navbar>
+            </div>
+        );
+    }
+}
+
+export default App;
+
+/*function App() {
   return (
     <div className="App">
       <header className="App-header">
@@ -22,4 +41,4 @@ function App() {
   );
 }
 
-export default App;
+export default App; */
